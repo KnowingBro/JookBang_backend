faff4b6c6549aa2ef0396837253c2e5e024e7fc0
Woongbin06 <kob05314@gmail.com>
2023-07-05 09:21:10 +0900

    프로젝트 설정

---
+++ src/main/java/com/knownbro/jookbang/JookbangApplication.java
@@ -0,0 +1,13 @@
+package com.knownbro.jookbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JookbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JookbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knownbro/jookbang/JookbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knownbro.jookbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JookbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
